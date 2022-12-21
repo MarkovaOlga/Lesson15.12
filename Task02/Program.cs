d264f3c4cce87d404997bece02647212da0142a7
Olga <olgamarkova85@ya.ru>
2022-12-21 20:23:22 +0300

    Добавили Task01, Task02, Task03

---
+++ Task02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max;
+
+if (number1 > number2) max = number1;
+else max = number2;
+
+Console.WriteLine($"{max} больше");
