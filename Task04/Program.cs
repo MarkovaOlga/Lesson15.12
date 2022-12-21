4e13cebe5b61eb23c705c9e74ee1a44b1259d35b
Olga <olgamarkova85@ya.ru>
2022-12-21 23:10:45 +0300

    Изменили Task04,Task06,добавили Task08

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -4,10 +4,9 @@ Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
-int max;
+int max = number1;
 
-if (number1 > number2 && number1 > number3) max = number1;
-else if (number2 > number1 && number2 > number3) max = number2;
-else max = number3;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
 
 Console.WriteLine($"Число {max} максимальное.");
--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number / 2 == 0)
-    Console.WriteLine($"Число {number} четное.");
-else Console.WriteLine($"Число {number} нечетное.");
+if (number % 2 == 0)
+    Console.WriteLine($"Число {number} является четным.");
+else Console.WriteLine($"Число {number} не является четным.");
