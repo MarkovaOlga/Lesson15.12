21ddabc8f3c0f519828bb81049837e56b64e63bc
Olga <olgamarkova85@ya.ru>
2022-12-21 22:43:09 +0300

    Добавили Task05,Task06,Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >=100 && number <1000)
+Console.WriteLine($"Последняя цифра числа {number} равна {number % 10}");
+else Console.WriteLine("Вы ввели не трехзначное число");
