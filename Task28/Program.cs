271d646210c2ef448d2867512ae11e19bd9fffc3
Olga <olgamarkova85@ya.ru>
2023-01-16 13:47:15 +0300

    Добавили Task24,Task26,Task28,Task30

---
+++ Task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int mult = Fact(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {mult}");
+
+int Fact(int num)
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+        mult = mult * i; //mult *=i
+        }
+    }
+    return mult;
+}
---
+++ Task30/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// int[] array = new int[8]; 
+// array[0]=235;
+// array[1]=33;
+// array[2]=56;
+
+// Console.WriteLine(array[]);
+
+// for (int i=0; i<array.Length; i++)
+// {
+//     array[i]=2;
+// }
+
+// int[] array = new int [5]{2,5,7,9,0};
+// int[] array = new int []{5,7};
+
+
+int[] array = CreateArray(10, 9, 23);
+PrintArray(array);
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(min, max);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(i < arr.Length - 1 ? arr[i] + ", " : arr[i] + "");
+    }
+    Console.Write("]");
+}
+
