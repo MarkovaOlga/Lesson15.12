baca75bbcfec8c9d553628cefc87b45216328f3d
Olga <olgamarkova85@ya.ru>
2023-01-22 01:11:08 +0300

    Добавили Task34,Task36,Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        double num = rnd.NextDouble() * (max - min) + min;
+        arr[i] = Math.Round(num, 1);
+    }
+    return arr;
+}
+
+void PrintArrayDouble(double[] arr)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+double MaxNumber(double[] arr)
+{
+    double maxNumber = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > maxNumber) maxNumber = arr[i];
+    }
+    return maxNumber;
+}
+
+double MinNumber(double[] arr)
+{
+    double minNumber = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < minNumber) minNumber = arr[i];
+    }
+    return minNumber;
+}
+
+
+double[] array = CreateArrayRndDouble(5, 10, 50);
+PrintArrayDouble(array);
+Console.WriteLine(MaxNumber(array) - MinNumber(array));
