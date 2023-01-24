38fc064a6a527695e46c9a22b69e8026b00f5fd0
Olga <olgamarkova85@ya.ru>
2023-01-24 12:26:30 +0300

    Добавили Task39,Task40,Task42,Task44,Task45

---
+++ Task40/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(IsExistTriangle(number1, number2, number3) ? "Такой треугольник может существовать" : "Такого треугольника не может существовать");
+
+bool IsExistTriangle(int num1, int num2, int num3)
+{
+    return num1 + num2 > num3 && num1 + num3 > num2 && num2 + num3 > num1;
+}
+
---
+++ Task45/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+int[] array = CreateArrayRndInt(9, 10, 100);
+PrintArray(array);
+int[] newarray = CopyOfArrayRndInt(array);
+//newarray[0] = 30;
+PrintArray(newarray);
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + " ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+
+int[] CopyOfArrayRndInt(int[] copyarr)
+{
+    int[] copyarr1 = new int[copyarr.Length];
+    for (int i = 0; i < copyarr1.Length; i++)
+    {
+        copyarr1[i] = array[i];
+    }
+    return copyarr1;
+}
+
+
+// string str ="1,4,6,8,0,-56";
+// string[] strArr = str.Split(',');
