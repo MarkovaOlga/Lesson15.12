5a40ee7fed21b09040eae0f03e3c97b89673b48e
Olga <olgamarkova85@ya.ru>
2023-01-25 11:48:03 +0300

    Добавили Task41,Task43

---
+++ Task41/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = EnterNumbers(num);
+PrintArray(array);
+int result = PositiveNumbers(array);
+Console.WriteLine($"-> {result}");
+
+int[] EnterNumbers(int m)
+{
+    int[] arr = new int[m];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} число из {m}: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        arr[i] = number;
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+}
+
+int PositiveNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите значение для b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение для k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение для b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение для k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double resultx = ValueOfX(b1, k1, b2, k2);
+resultx = Math.Round(resultx, 2);
+double resulty = ValueOfY(resultx, b2, k2);
+resulty = Math.Round(resulty, 2);
+
+Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({resultx}; {resulty})");
+
+double ValueOfX(double bb1, double kk1, double bb2, double kk2)
+{
+    double xx = (bb2 - bb1) / (kk1 - kk2);
+    return xx;
+}
+double ValueOfY(double x1, double bb2, double kk2)
+{
+    double yy = kk2 * x1 + bb2;
+    return yy;
+}
