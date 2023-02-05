8e8bc74c1c11ee42eaac656a54b1c27dabd092f0
Olga <olgamarkova85@ya.ru>
2023-02-05 19:32:19 +0300

    Добавили Task63,Task65,Task67,Task69

---
+++ Task65/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+Console.WriteLine("Введите первое натуральное число: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе натуральное число: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbersRange(firstnumber, secondnumber);
+
+void NaturalNumbersRange(int firstnum, int secondnum)
+{
+    if (firstnum == secondnum) Console.WriteLine($"{firstnum} ");
+    if (firstnum < secondnum)
+    {
+        Console.Write($"{firstnum} ");
+        NaturalNumbersRange(firstnum + 1, secondnum);
+    }
+    if (firstnum > secondnum)
+    {
+        Console.Write($"{firstnum} ");
+        NaturalNumbersRange(firstnum - 1, secondnum);
+    }
+}
---
+++ Task69/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int numdegree = Convert.ToInt32(Console.ReadLine());
+
+if (numdegree > 0)
+{
+    Console.WriteLine(MathPow(number, numdegree));
+}
+else Console.WriteLine("Ошибка ввода степени");
+
+int MathPow(int num, int degree)
+{
+    if (degree == 0) return 1;
+    else return num * MathPow(num, degree - 1);
+}
